fc165f3f1413b84cebcace0d265f7f4b3bb1e84d
paul <paul@pop-os.localdomain>
2020-05-26 14:23:34 +0200

    delete button

--- connexion.php
+++ connexion.php
@@ -12,8 +12,7 @@
         <button name="co_user">connexion utilisateur</button>
         <button name="new_user">nouveau compte utilisateur</button>
         <button name="co_admin">connexion admin</button>
-        <button name="co_comp">connexion entreprise</button>
-        <button name="new_comp">nouveau compte entreprise</button>
+
     </form>
 
 
@@ -24,20 +23,14 @@
 </html>
 
 <?php
-if (isset($_POST['co_user'])){
-    header('Location: ./demandeur/connexion_demandeur.php');
-}
-if (isset($_POST['new_user'])){
-    header('Location: ./demandeur/new_demandeur.php');
-}
 if (isset($_POST['co_admin'])){
     header('Location: ./admin/connexion_admin.php');
 }
-if (isset($_POST['co_comp'])){
-    header('Location: ./donneur/connexion_donneur.php');
+if (isset($_POST['new_user'])){
+    header('Location: ./users/new_users.php');
 }
-if (isset($_POST['new_comp'])){
-    header('Location: ./donneur/new_donneur.php');
+if (isset($_POST['co_user'])){
+    header('Location: ./users/connexion_users.php');
 }
 
 ?>
